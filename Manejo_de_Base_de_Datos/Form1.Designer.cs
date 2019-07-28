17f162c7b64f6006035cf541650c2fd1d50e7d0c
William Gonzalez <wjga2001.unitec@gmail.com>
2019-07-27 23:29:43 -0430

    Entrega Final

--- Manejo_de_Base_de_Datos/Form1.Designer.cs
+++ Manejo_de_Base_de_Datos/Form1.Designer.cs
@@ -33,12 +33,13 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 14);
+            this.label1.Location = new System.Drawing.Point(81, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(221, 13);
             this.label1.TabIndex = 0;
@@ -46,7 +47,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(28, 50);
+            this.button1.Location = new System.Drawing.Point(62, 50);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(104, 49);
             this.button1.TabIndex = 1;
@@ -56,7 +57,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(162, 50);
+            this.button2.Location = new System.Drawing.Point(214, 50);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(104, 49);
             this.button2.TabIndex = 2;
@@ -66,7 +67,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(28, 122);
+            this.button3.Location = new System.Drawing.Point(62, 122);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(104, 49);
             this.button3.TabIndex = 3;
@@ -76,7 +77,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(162, 122);
+            this.button4.Location = new System.Drawing.Point(214, 122);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(104, 49);
             this.button4.TabIndex = 4;
@@ -84,11 +85,21 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(355, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Programa creado por William Gonzalez, Hector Vasquez y Yohana Pineda";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(288, 186);
+            this.ClientSize = new System.Drawing.Size(383, 212);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -110,6 +121,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
     }
 }
 
